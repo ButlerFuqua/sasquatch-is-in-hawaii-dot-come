f392e73af4e1b354ede0d571c6926f69d7e7f3ed
butlerfuqua <butlerfuqua@gmail.com>
2024-08-22 19:58:34 -1000

    Add meta tags and favicon file

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,13 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const siteTitle = 'Sasquatch is in Hawaii';
+const siteDescription = 'A short film about a self-absorbed media guru and an Alabama Squatcher try to find Sasquatch in the one location no one has looked -Hawaii';
+const thumbnailPath = `https://www.sasquatchisinhawaii.com/thumbnail`;
+
 export const metadata: Metadata = {
-  title: "Sasquatch is in Hawai'i",
-  description: "A short film about a self-obsorbed media guru and an Alabama Squatcher try to find Sasquatch in the one location no one has looked -Hawai'i",
+  title: siteTitle,
+  description: siteDescription,
 };
 
 export default function RootLayout({
@@ -16,6 +20,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#09a404" />
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#09a404" />
+
+
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:type" content="video.movie" />
+      <meta property="og:url" content="https://www.sasquatchisinhawaii.com" />
+      <meta property="og:image" content={thumbnailPath} />
+
+      <meta property="og:description" content={siteDescription} />
+
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDescription} />
+      <meta name="twitter:image" content={thumbnailPath} />
+      <meta name="twitter:card" content="summary_large_image" />
+
+      <meta property="og:site_name" content={siteTitle}></meta>
+      <meta name="twitter:image:alt" content="Sasquatch is in Hawaii Movie Poster"></meta>
+
+
+
       <body className={inter.className}>
         {children}
         <footer className="bg-gray_dark text-white">
